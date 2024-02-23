612411ebb001aa39ed2617fbba7320070be3d623
Ghost233 <only.yesc@gmail.com>
2024-02-23 11:36:34 +0800

    init

---
+++ app/src/main/java/me/ghost233/ghosttaskremind/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package me.ghost233.ghosttaskremind.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
